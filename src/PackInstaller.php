df66f66c117677e201ec9f3e8186015744c72848
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2024-02-11 06:28:18 +0100

    created logic for installing dependencies

--- src/PackInstaller.php
+++ src/PackInstaller.php
@@ -9,10 +9,31 @@ use Composer\Repository\InstalledRepositoryInterface;
 
 class PackInstaller extends LibraryInstaller
 {
+    private static string $dependenciesFilePath;
+
+    public static function getDependenciesFilePath(): string
+    {
+        return self::$dependenciesFilePath;
+    }
+
+    public static function setDependenciesFilePath($dependenciesFilePath): void
+    {
+        self::$dependenciesFilePath = $dependenciesFilePath;
+    }
+
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        echo $package->getPrettyName();
-        echo $package->getName();
+        $velsym_dependencies_path = realpath($this->getInstallPath($package)."/velsym-dependencies/dependencies.php");
+        $dependenciesFileArray = file(self::$dependenciesFilePath);
+        $lastKey = array_key_last($dependenciesFileArray) ?? -1;
+        $dependenciesFileArray[$lastKey] = "    require('$velsym_dependencies_path'),\n";
+        $dependenciesFileArray[$lastKey+1] = "];";
+        file_put_contents(self::$dependenciesFilePath, $dependenciesFileArray);
         return parent::install($repo, $package);
+    }
+
+    public function supports(string $packageType)
+    {
+        return "velsym-pack" === $packageType;
     }
 }
